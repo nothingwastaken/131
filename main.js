98fa8d3c3a0629de0dfb09bd57283c4af1bceb5f
nothingwastaken <82754118+nothingwastaken@users.noreply.github.com>
2023-02-16 18:02:28 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -25,17 +25,6 @@ function modelLoaded(){
 
 function draw(){
     image(video, 0, 0, 380, 380);
-    fill("#FF0000");
-    text("Dog", 45, 70);
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350);
-
-    fill("#FF0000");
-    text("Cat", 320, 120);
-    noFill();
-    stroke("#FF0000");
-    rect(300, 90, 270, 320);
 
     if (status != ""){
         r = random(225);
